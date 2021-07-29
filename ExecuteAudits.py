43dd2da0daa0408f0ccde45167aa17cd3b71cefa
Archan Das <creeksidedas@gmail.com>
2021-07-29 16:26:22 -0500

    it does audits! really fast!

--- ExecuteAudits.py
+++ ExecuteAudits.py
@@ -4,13 +4,17 @@ import pandas as pd
 
 # input_deep_link = sys.argv[1]
 
-infile = "C:/Users/creek/Desktop/WowBaoScripts/MenuAuditAutomation/SomeLocations.csv"
-outfile = "C:/Users/creek/Desktop/WowBaoScripts/MenuAuditAutomation/AuditOutput.csv"
+infile = "C:/Users/creek/Desktop/WowBaoScripts/MenuAuditAutomation/testAudits.csv"
+outfile = "C:/Users/creek/Desktop/WowBaoScripts/MenuAuditAutomation/AuditOutputTest.csv"
 def audit(address, deep_link, isCheeseburgerBao, isCoconutBao, isEggSausageBao, isIMPOSSIBLEBao, isBundles, isPotDumpCombined):
     menu = Menu(address, None)
-    temp = BuildMenuTemplate('C:/Users/creek/Desktop/WowBaoScripts/MenuAuditAutomation/MenuReference.json', isCheeseburgerBao, isCoconutBao, isEggSausageBao, isIMPOSSIBLEBao, isBundles, isPotDumpCombined)
-
+    temp = BuildMenuTemplate('C:/Users/creek/Desktop/WowBaoScripts/MenuAuditAutomation/MenuReference.json', isCheeseburgerBao, isCoconutBao, isEggSausageBao, isIMPOSSIBLEBao, isBundles, False)#isPotDumpCombined)
     menu.loadItems(deep_link)
+
+    if "Potstickers and Dumplings" in menu.categories:
+        temp.categories.remove("Pan-Seared Potstickers")
+        temp.categories.remove("Steamed Dumplings")
+        temp.categories.append("Potstickers and Dumplings")
 
     menu.template_menu = temp
 
@@ -26,9 +30,18 @@ locations = pd.read_csv(infile)
 rows = []
 
 for location in locations.iterrows():
-    try:
+    #try:
         address = location[1]["Store Address"] + " " + location[1]["Store City"] + " " + location[1]["Store State"] + " " + str(location[1]["Store Zip"])
-        issues = audit(address, location[1]["DD Deep Link"])
+        menu_params = {
+            "isCheeseburgerBao":("Yes" == location[1].get("Cheeseburger Bao", "No")),
+            "isCoconutBao":("Yes" == location[1].get("Cheeseburger Bao", "No")),
+            "isEggSausageBao":("Yes" == location[1].get("Cheeseburger Bao", "No")),
+            "isIMPOSSIBLEBao":("Yes" == location[1].get("Cheeseburger Bao", "No")),
+            "isBundles":("Yes" == location[1].get("Cheeseburger Bao", "No")),
+            ##TODO: figure out a better way to handle combined potsticker/dumpling category
+            "isPotDumpCombined":False
+        }
+        issues = audit(address, location[1]["DD Deep Link"], menu_params["isCheeseburgerBao"], menu_params["isCoconutBao"], menu_params["isEggSausageBao"], menu_params["isIMPOSSIBLEBao"], menu_params["isBundles"], menu_params["isPotDumpCombined"])
         dict = {
             'Opportunity ID':location[1]["Opportunity ID"],
             'Opportunity Name':location[1]["Opportunity Name"],
@@ -39,7 +52,7 @@ for location in locations.iterrows():
         }
         rows.append(dict)
         print("successfully audited " + location[1]["Opportunity Name"] + ": found " + str(len(issues)) + " issues")
-    except:
+    #except:
         print("exception while auditing " + location[1]["Opportunity Name"])
 
 df_output = pd.DataFrame(rows)
